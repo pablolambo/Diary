99a2c6a48e50d2d3401d268b873ba28e9123667d
Paul Frank <pawelfrankowski42@gmail.com>
2025-02-05 18:31:21 +0100

    Fixes for tags relation

--- Diary.Domain/Entities/TagEntity.cs
+++ Diary.Domain/Entities/TagEntity.cs
@@ -4,7 +4,7 @@ public class TagEntity
 {
     public Guid Id { get; set; }
     public Guid EntryEntityId { get; set; }
-    public EntryEntity? EntryEntity { get; set; }
+    public List<EntryEntity>? EntryEntity { get; set; }
     public string Name { get; set; } = string.Empty;
     public string UserId { get; set; } = string.Empty;
 }
